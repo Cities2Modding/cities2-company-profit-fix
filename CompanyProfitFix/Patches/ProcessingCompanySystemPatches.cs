88071fd0c202c56add15508e8fbd19c37bd23170
89pleasure <133201120+89pleasure@users.noreply.github.com>
2024-01-30 20:36:45 +0100

    Initial release

---
+++ CompanyProfitFix/Patches/ProcessingCompanySystemPatches.cs
@@ -0,0 +1,49 @@
+﻿using Game.Companies;
+using Game.Economy;
+using Game.Prefabs;
+using Game.Simulation;
+using HarmonyLib;
+using Unity.Entities;
+using Unity.Mathematics;
+
+namespace CompanyProfitFix.Patches
+{
+    [HarmonyPatch(typeof(ProcessingCompanySystem), "EstimateDailyProfit")]
+    public static class ProcessingCompanySystemPatches
+    {
+        [HarmonyPatch([typeof(int), typeof(int), typeof(IndustrialProcessData), typeof(EconomyParameterData), typeof(DynamicBuffer<TradeCost>), typeof(ResourcePrefabs), typeof(ComponentLookup<ResourceData>)], [ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Ref, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal])]
+        [HarmonyPrefix]
+        public static bool Prefix(int production, int wages, IndustrialProcessData processData, ref EconomyParameterData economyParameters, DynamicBuffer<TradeCost> tradeCost, ResourcePrefabs resourcePrefabs, ComponentLookup<ResourceData> resourceDatas, ref int __result)
+        {
+            TradeCost tradeCost2 = EconomyUtils.GetTradeCost(processData.m_Output.m_Resource, tradeCost);
+            float3 tradeCost3 = default;
+            tradeCost3.x = 0f;
+            if (float.IsNaN(tradeCost2.m_BuyCost))
+            {
+                tradeCost3.x = tradeCost2.m_SellCost - 0f;
+            }
+            else if (float.IsNaN(tradeCost2.m_SellCost))
+            {
+                tradeCost3.x = 0f - tradeCost2.m_BuyCost;
+            }
+            else
+            {
+                tradeCost3.x = tradeCost2.m_SellCost - tradeCost2.m_BuyCost;
+            }
+            tradeCost3.y = EconomyUtils.GetTradeCost(processData.m_Input1.m_Resource, tradeCost).m_BuyCost;
+            tradeCost3.z = EconomyUtils.GetTradeCost(processData.m_Input2.m_Resource, tradeCost).m_BuyCost;
+
+            // Fixed calculate profit
+            __result = ProcessingCompanySystem.EstimateDailyProfit(production, wages, processData, ref economyParameters, tradeCost3, resourcePrefabs, resourceDatas);
+
+            // Do not execute original method
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(TransportBoardingHelpers))]
+    public static class TransferFixPatch
+    {
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+# CompanyProfitFix for Cities Skylines II
+
+## Features:
+- Fixes the subsidies breaking introduced with 1.0.18f
+- Fixes NaN values creating a high negative estimated profit for companies such as commercials, offices and industrials.
+- The fix may address several issues in multiple areas of the simulation that we cannot measure such as rent, land value, or export and import.
+
+## Important:
+- **The fixed calculation needs a while to take effect. Please stay calm.**
+- **The mod is activated globally**
+- **It has an NO impact on your save. You can safely use this mod and remove it at anytime.**
+
+## Requirement
+- [BepInEx v5](https://thunderstore.io/c/cities-skylines-ii/p/BepInEx/BepInExPack/)
+
+## Mod manager installation
+I do recommend using the [Thunderstore.io mod manager](https://www.overwolf.com/app/Thunderstore-Thunderstore_Mod_Manager) , or [r2modman](https://thunderstore.io/package/ebkr/r2modman/) for Game Pass users
+
+## Community, Feedback & Support
+If you need any help or are you interested in modding yourself?
+Join our discord community: [https://discord.gg/M9rgRtGcRa](https://discord.gg/M9rgRtGcRa)
+
+## Credits
+- **Rebecca** for original idea, code and fixing the issue
+- **89pleasure** for patching and creating a mod
+
+## License:
+- This mod is subject to the GNU license. Please read the license terms before using the mod.
